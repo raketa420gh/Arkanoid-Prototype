470e4650adaffab4e6a2aceaded5c3fa09e0cd54
Andrei Raketa <emmmfu@gmail.com>
2021-05-20 05:31:43 +0300

    no message

---
+++ Assets/PickUpScore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PickUpScore : MonoBehaviour
+{
+    #region Unity lifecycle
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag(Tags.Pad))
+        {
+            Debug.Log($"Вы подняли {gameObject}");
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+}
--- Assets/Scripts/Tags.cs
+++ Assets/Scripts/Tags.cs
@@ -1,4 +1,6 @@
 ﻿public static class Tags
 {
     public const string Ball = "Ball";
+    public const string Pad = "Pad";
+    public const string PickUp = "PickUp";
 }
